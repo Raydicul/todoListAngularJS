0f0a6a7242a802629185ab444d2fd47d8683cd0b
Florian Billot <florian.billot@gmail.com>
2015-02-20 09:39:28 +0100

    Ajout édition et filtre des todo

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 var app = angular.module('todo', []);
 
+
+
 app.directive('ngBlur', function(){
+	
 	return function(scope, elem, attrs){
-		console.log(elem);
-		console.log(attrs);
+	elem.bind('blur', function(){
+		scope.$apply(attrs.ngBlur)
+	})
 	}
 
 })
 
-app.controller('TodoCtrl', function($scope, filterFilter, $http){
+app.controller('TodoCtrl', function($scope, filterFilter, $http, $location){
 
 	$scope.todos = [];
 	$scope.placeholder = "Chargement...";
+	$scope.statusFilter = {};
 
 	$http.get('todo.php').success(function(data){
 		$scope.todos = data;
@@ -22,6 +27,16 @@ app.controller('TodoCtrl', function($scope, filterFilter, $http){
 		$scope.remaining = filterFilter($scope.todos, {completed:false}).length;
 		$scope.allchecked != $scope.remaining;
 	}, true)
+	
+	if($location.path() == ''){ $location.path('/')}
+	$scope.location = $location;
+	$scope.$watch('location.path()', function(path) {
+		$scope.statusFilter = 
+		(path == '/active') ? {completed : false} :
+		(path == '/done') ? {completed : true} :
+		null;
+	    
+	});
 
 	$scope.removeTodo = function(index){
 		$scope.todos.splice(index, 1);
